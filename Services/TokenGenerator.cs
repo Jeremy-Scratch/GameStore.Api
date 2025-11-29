60daf04343ea5c254d7ae07fd01ffc1e9ffab41d
Scratch <Jeremyscratch6@gmail.com>
2025-11-29 16:48:23 -0400

    Add: Service to generate Session-Token

---
+++ Services/TokenGenerator.cs
@@ -0,0 +1,18 @@
+using System.Security.Cryptography;
+
+namespace GameStore.Api.Services;
+
+public static class TokenGenerator
+{
+    public static string GenerateToken()
+    {
+        var randomNumber = new byte[32];
+
+        using( var rgn = RandomNumberGenerator.Create())
+        {
+            rgn.GetBytes(randomNumber);
+        }
+        return Convert.ToBase64String(randomNumber);
+    }
+
+}
